b859e9f58b480b2c9339d7f0fe183021bf307488
Wesley Hughes <wesley_hughes@me.com>
2023-02-08 11:40:01 -0600

    not really sure what just happened

--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -1,6 +1,36 @@
-import { getUsers, getFavorites } from "../data/provider.js"
+import { getUsers, getFavorites, setChosenUser } from "../data/provider.js"
 
 export const Footer = () => {
+const users = getUsers()
+const favorites = getFavorites()
+const currentUser = parseInt(localStorage.getItem("gg_user"))
 
-    
+return `
+<div> <label for="postsByUser">Posts by user</label>
+<select id="footerUser">
+    <option value="0" >Choose...</option>
+${users.map(user => {
+    return `<option class="filterPostByUser" value="${user.id}">${user.name}</option>`
+})}
+</select></div>
+
+<div>Show only favorites <input type="checkbox" id="showFavorites" value="${currentUser}"/> `   
 }
+
+
+addEventListener("change", event => {
+    if (event.target.id === "footerUser") {
+        const userId = document.querySelector("#footerUser").value
+        setChosenUser(parseInt(userId))
+        dispatchEvent(new CustomEvent("stateChanged"))
+
+    }
+})
+
+
+addEventListener("change", event => {
+    if (event.target.id === "showFavorites") {
+        const currentUserId = parseInt(document.querySelector("input[type='checkbox']:checked").value)
+        matchedUserFavorites(currentUserId)
+    }
+})
